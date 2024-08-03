d77f69fbbd9069cfed399f384cb17014801cc4d9
ppooll1 <ricklazarmusic@hotmail.com>
2024-08-03 05:07:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_s7k1v6m", "template_38b110w", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
